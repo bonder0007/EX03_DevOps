a078f5573948aea2681dffd5a3356448c941bbef
Onn Bonderman <bonder0007>
2024-12-14 18:25:56 +0200

    final changing

--- HelloProgram.java
+++ HelloProgram.java
@@ -4,7 +4,7 @@ public class HelloProgram
     {
         for (int i=1;i<=10; i++) 
         {
-            System.out.println("Changed as requested #after changeingProgram  ..."+i);
+            System.out.println("Changed as requested #Final for GOd sake  ..."+i);
         }
     }
 } 
